a03dab52d45d2f252c40cc28a431ab5bd75d6f7b
Dulithi Amunugama <dulithi.a@gmail.com>
2025-07-06 14:33:57 +0530

    Day 2

--- main.py
+++ main.py
@@ -2,22 +2,14 @@ import random
 
 #functions
 
-def create_board():
+def create_board(number):
     for row in range(3):
         for coulmn in range(3):
             print(f"| {number}", end=' ')  
             number += 1
-        print()
+        print("|")
 
 
-def get_player_X_inputs():
-    print("This is X turn.")
-    playerX = int(input("Input your move (0-8): "))
-
-
-def get_player_O_inputs():
-    playerO = random.randint(0,8)
-    print(f"O move to {playerO}")
 
 
 def check_winner(turn):
@@ -31,8 +23,10 @@ def check_winner(turn):
 #statments
 
 board = [] 
-number = 0
+location_number = 0
 
-create_board()
+create_board(location_number)
 
-num_of_turns = 0   
+num_of_turns = 0
+
+
